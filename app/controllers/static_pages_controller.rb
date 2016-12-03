d166427f5fa3dc91f11591f447cfc9383eb7145a
leon20121005 <leon20121005@gmail.com>
2016-12-03 19:05:58 +0800

    first commit (modeling users finished)

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def mitaliang
+  end
+
+  def niniko_w
+  end
+end
